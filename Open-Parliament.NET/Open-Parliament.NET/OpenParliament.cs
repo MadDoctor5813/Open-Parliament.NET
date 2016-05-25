85e1d8fb55dfd5d1cd4d6105460e25fef2efe487
Kevin <dragn194@gmail.com>
2016-05-24 22:49:01 -0400

    Add OpenParliament API class stub

---
+++ Open-Parliament.NET/Open-Parliament.NET/OpenParliament.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Open_Parliament.NET
+{
+    public class OpenParliament
+    {
+
+        private WebClient client;
+
+        public OpenParliament(string userAgent)
+        {
+            createClient();
+        }
+
+        private void createClient()
+        {
+            client = new WebClient();
+            client.Headers["User-Agent"] = userAgent;
+        }
+
+    }
+}
