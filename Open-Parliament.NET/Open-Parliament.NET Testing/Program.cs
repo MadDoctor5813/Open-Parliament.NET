4d20accda5793f894278b1c26a40d93b1a1ee454
Kevin <dragn194@gmail.com>
2016-07-04 14:03:59 -0400

    Add a testing project to the solution

---
+++ Open-Parliament.NET/Open-Parliament.NET Testing/Program.cs
@@ -0,0 +1,17 @@
+﻿using Open_Parliament.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Open_Parliament.NET_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OpenParliament openParliament = new OpenParliament("OpenParliament.NET - Testing - dragn194@gmail.com");
+
+        }
+    }
+}
